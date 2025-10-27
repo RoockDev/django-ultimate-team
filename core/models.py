71189e41fe5faa2d9e5f29efa0017df76e756072
RoockDev <zar0_31@outlook.com>
2025-10-27 20:58:37 +0100

    feat: Definir modelos base de la aplicación
    Se crean los modelos País, Liga, Club, Usuario, Carta_jugador y Equipo_usuario para establecer la estructura de la base de datos. Se incluye la relación ManyToMany para las plantillas de los equipos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,75 @@
+from django.db.models import CASCADE
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
 
-# Create your models here.
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=100,unique=True,verbose_name="País")
+
+    def __str__(self):
+        return self.nombre
+
+
+class Liga(models.Model):
+    nombre = models.CharField(max_length=100,unique=True,verbose_name="Liga")
+
+    def __str__(self):
+        return self.nombre
+
+class Club(models.Model):
+    nombre = models.CharField(max_length=100,unique=True,verbose_name="Nombre Club")
+    pais = models.ForeignKey(Pais,on_delete=models.CASCADE)
+    liga = models.ForeignKey(Liga,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    username = models.CharField(max_length=150, unique=True,verbose_name="Nombre de Usuario")
+    email = models.EmailField(unique=True,verbose_name="Correo Electrónico")
+    password = models.CharField(max_length=8, verbose_name="Contraseña")
+    nombre = models.CharField(max_length=100,blank=True,verbose_name="Nombre")
+    apellidos = models.CharField(max_length=100,blank=True,verbose_name="Apellidos")
+    fecha_nacimiento = models.DateField(null=True,blank=True)
+
+    def __str__(self):
+        return self.username
+
+class Carta_jugador(models.Model):
+
+    POSICIONES = [
+        ('POR', 'Portero'),
+        ('DFC', 'Defensa Central'),
+        ('LTI', 'Lateral Izquierdo'),
+        ('LTD', 'Lateral Derecho'),
+        ('MC', 'Medio Centro'),
+        ('MI', 'Medio Izquierdo'),
+        ('MD', 'Medio Derecho'),
+        ('DC', 'Delantero Centro'),
+        ('MP', 'Media Punta'),
+    ]
+    nombre = models.CharField(max_length=100,verbose_name="Nombre Jugador", null=False)
+    pais = models.ForeignKey(Pais,on_delete=models.CASCADE,null=False)
+    posicion = models.CharField(max_length=3, choices=POSICIONES,verbose_name="Posicion")
+    ritmo = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(99)],null=False,verbose_name="Ritmo")
+    tiro = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(99)],null=False,verbose_name="Tiro")
+    pase = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(99)],null=False,verbose_name="Pase")
+    regate = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(99)],null=False,verbose_name="Regate")
+    defensa = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(99)],null=False,verbose_name="Defensa")
+    fisico = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(99)],null=False,verbose_name="Físico")
+    liga = models.ForeignKey(Liga,on_delete=models.CASCADE,null=False)
+    club = models.ForeignKey(Club,on_delete=models.CASCADE,null=False)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Equipo_usuario(models.Model):
+    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE,null=False)
+    nombre = models.CharField(max_length=100,verbose_name="Nombre Equipo")
+    fecha_creacion = models.DateField(auto_now_add=True)
+    cartas = models.ManyToManyField(Carta_jugador)
+
+    def __str__(self):
+        return self.nombre
+
