acb1750bbf5d732578f24c276ad60f5a5b7870bf
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-28 00:40:33 +0100

    feat: Implementar la lectura (GET) de cartas
    Se crea la vista 'listar_cartas' y la URL '/api/cartas/' para obtener un listado de todas las cartas en formato JSON. La vista maneja el caso de no encontrar resultados devolviendo un mensaje específico.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
 
-# Create your views here.
+def listar_cartas(request):
+    cartas = list(Carta_jugador.objects.values(
+        'id',
+        'nombre',
+        'pais__nombre',
+        'posicion',
+        'ritmo',
+        'tiro',
+        'pase',
+        'regate',
+        'defensa',
+        'fisico',
+        'liga__nombre',
+        'club__nombre',
+        'activo'
+    ))
+
+    if cartas:
+        return  JsonResponse(cartas, safe=False)
+    else:
+        return  JsonResponse({'mensaje': 'No se encontraron cartas en la base de datos'})
