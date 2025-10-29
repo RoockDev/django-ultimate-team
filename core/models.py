7d5f39d94dd6c34f31d8ccc6fd46a6a7b316edc9
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-29 23:56:29 +0100

    feat: Implementar logica de calculo de puntuacion.
    Se implementa la lógica para el cálcula de la puntuación final de la carta, controlando si es un portero o es de otra posición, se sobreescribe el método save con el algortimo de cálculo de valoración

--- core/models.py
+++ core/models.py
@@ -34,7 +34,23 @@ class Usuario(models.Model):
 
     def __str__(self):
         return self.username
+
+
+
 VALIDADORES_STATS = [MinValueValidator(1), MaxValueValidator(99)]
+class Atributos_portero(models.Model):
+    carta = models.OneToOneField('Carta_jugador', on_delete=models.CASCADE, primary_key=True,verbose_name="Carta de Jugador")
+    salto = models.IntegerField(validators=VALIDADORES_STATS,verbose_name="Salto")
+    parada = models.IntegerField(validators=VALIDADORES_STATS,verbose_name="Parada")
+    saque = models.IntegerField(validators=VALIDADORES_STATS,verbose_name="Saque")
+    reflejos = models.IntegerField(validators=VALIDADORES_STATS, verbose_name="Reflejos")
+    velocidad = models.IntegerField(validators=VALIDADORES_STATS,verbose_name="Velocidad Portero")
+    posicionamiento = models.IntegerField(validators=VALIDADORES_STATS,verbose_name="Posicionamiento Portero")
+
+    def __str__(self):
+     return f"Atributos de Portero para {self.carta.nombre}"
+
+
 class Carta_jugador(models.Model):
 
     POSICIONES = [
@@ -62,21 +78,65 @@ class Carta_jugador(models.Model):
     activo = models.BooleanField(default=True)
     puntuacion_total = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(99)],verbose_name="Puntuación General")
 
+
+
     def __str__(self):
         return self.nombre
 
+    def calcular_valoracion_base(self):
+        if self.posicion == 'POR':
 
-class Atributos_portero(models.Model):
-    carta = models.OneToOneField('Carta_jugador', on_delete=models.CASCADE, primary_key=True,verbose_name="Carta de Jugador")
-    salto = models.IntegerField(validators=VALIDADORES_STATS,verbose_name="Salto")
-    parada = models.IntegerField(validators=VALIDADORES_STATS,verbose_name="Parada")
-    saque = models.IntegerField(validators=VALIDADORES_STATS,verbose_name="Saque")
-    reflejos = models.IntegerField(validators=VALIDADORES_STATS, verbose_name="Reflejos")
-    velocidad = models.IntegerField(validators=VALIDADORES_STATS,verbose_name="Velocidad Portero")
-    posicionamiento = models.IntegerField(validators=VALIDADORES_STATS,verbose_name="Posicionamiento Portero")
+                atributos_por = self.atributos_portero
 
-    def __str__(self):
-     return f"Atributos de Portero para {self.carta.nombre}"
+                stats_relevantes = [
+                    atributos_por.salto,
+                    atributos_por.parada,
+                    atributos_por.saque,
+                    atributos_por.reflejos,
+                    atributos_por.velocidad,
+                    atributos_por.posicionamiento
+                ]
+                media_base = sum(stats_relevantes) / len(stats_relevantes)
+        else:
+            stats_relevantes = [self.ritmo, self.tiro, self.pase, self.regate, self.defensa, self.fisico]
+            media_base = sum(stats_relevantes) / len(stats_relevantes)
+            return round(media_base)
+
+    def calcular_bonificacion(self):
+
+        bonificacion = 0
+        stats_totales = [self.ritmo, self.tiro, self.pase, self.regate, self.defensa, self.fisico]
+
+        for stat in stats_totales:
+            if stat > 95:
+                bonificacion += 4
+            elif stat > 90:
+                bonificacion += 3
+            elif stat > 80:
+                bonificacion += 2
+            elif stat >= 50:
+                bonificacion += 1
+            else:
+                bonificacion -= 1
+
+        return bonificacion
+
+    def save(self, *args, **kwargs):
+
+        puntuacion_base = self.calcular_valoracion_base()
+        bonificacion = self.calcular_bonificacion()
+
+        puntuacion_final = puntuacion_base + bonificacion
+
+        if puntuacion_final > 99:
+            puntuacion_final = 99
+        elif puntuacion_final < 50:
+            puntuacion_final = 50
+
+        self.puntuacion_total = puntuacion_final
+        super().save(*args, **kwargs)
+
+
 
 
 class Equipo_usuario(models.Model):
