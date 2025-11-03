1414f18584ffd7cda2fbd9bcc4d9b600fbf05b07
CarlosRamirezSantos <carlosramii2304@gmail.com>
2025-11-03 13:37:34 +0100

    feat: Implementar consulta del equipo de un usuario
    Se ha implementado la funcionalidad de consultar el equipo de un usuario. La respuesta incluye los detalles del equipo
    y la lista de cartas que contiene.

--- core/urls.py
+++ core/urls.py
@@ -17,5 +17,6 @@ urlpatterns = [
    path('usuarios/actualizar_especifica/<int:usuario_id>/', views.actualizar_campos_especificos_usuario),
    path('usuarios/borrar/<int:usuario_id>/', views.borrar_usuario),
    path('equipo/asignar/<int:usuario_id>/',views.asignar_equipo_a_usuario),
-   path('equipo/eliminar/<int:usuario_id>/',views.eliminar_equipo_de_usuario)
+   path('equipo/eliminar/<int:usuario_id>/',views.eliminar_equipo_de_usuario),
+   path('equipo/consultar/<int:usuario_id>/', views.consultar_equipo_usuario)
 ]
