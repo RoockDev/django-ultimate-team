c16412f709daae45a2022efb1eb0516e92db50c0
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-11-07 16:38:10 +0100

    test: añadir prueba unitaria para fallo de límite de porteros

--- core/tests.py
+++ core/tests.py
@@ -93,3 +93,70 @@ class PruebasModeloEquipo(TestCase):
             mensaje,
             "El equipo tiene 22 jugadores activos, debe tener entre 23 y 25"
         )
+
+
+
+
+
+    def test_validar_plantilla_falla_por_limite_porteros(self):
+
+            usuario_prueba = Usuario.objects.create(
+                username='testuser_porteros',
+                email='test_por@test.com',
+                password='123'
+            )
+            equipo_prueba = Equipo_usuario.objects.create(
+                usuario=usuario_prueba,
+                nombre="Equipo Test Porteros"
+            )
+            pais_prueba = Pais.objects.create(nombre="País de Prueba POR")
+            liga_prueba = Liga.objects.create(nombre="Liga de Prueba POR")
+            club_prueba = Club.objects.create(
+                nombre="Club de Prueba POR",
+                pais=pais_prueba,
+                liga=liga_prueba
+            )
+
+
+            cartas_de_prueba = []
+
+
+            for i in range(4):
+                carta = Carta_jugador.objects.create(
+                    nombre=f"Portero de Prueba {i + 1}",
+                    pais=pais_prueba,
+                    liga=liga_prueba,
+                    club=club_prueba,
+                    posicion='POR'
+                )
+                cartas_de_prueba.append(carta)
+
+
+            for i in range(21):
+                carta = Carta_jugador.objects.create(
+                    nombre=f"Delantero de Prueba {i + 1}",
+                    pais=pais_prueba,
+                    liga=liga_prueba,
+                    club=club_prueba,
+                    posicion='DC'
+                )
+                cartas_de_prueba.append(carta)
+
+
+            equipo_prueba.cartas.set(cartas_de_prueba)
+
+
+
+            es_valido, mensaje = equipo_prueba.validar_plantilla()
+
+
+
+            self.assertFalse(es_valido, "La validación debería fallar por 4 porteros, pero devolvió True.")
+
+
+            self.assertEqual(
+                mensaje,
+                "El equipo tiene 4 porteros. Debe tener entre 2 y 3."
+            )
+
+
