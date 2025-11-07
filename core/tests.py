ce9df3136104a6d02d6e402cdc8e4d8d372bfdb2
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-11-07 17:43:12 +0100

    test: tests de puntuación

--- core/tests.py
+++ core/tests.py
@@ -216,9 +216,133 @@ class PruebasModeloEquipo(TestCase):
 
                 es_valido, mensaje = equipo_prueba.validar_plantilla()
 
-                self.assertTrue(es_valido, f"La validación debería pasar (True), pero falló y devolvió: {mensaje}")
+
 
                 self.assertEqual(
                     mensaje,
                     "Plantilla validada"
                 )
+
+class PruebasModeloCarta(TestCase):
+
+    def setUp(self):
+
+       # esto lo creo porque esto lo voy a utilizar en los demas test
+
+        self.pais_prueba = Pais.objects.create(nombre="País de Prueba Cartas")
+        self.liga_prueba = Liga.objects.create(nombre="Liga de Prueba Cartas")
+        self.club_prueba = Club.objects.create(
+            nombre="Club de Prueba Cartas",
+            pais=self.pais_prueba,
+            liga=self.liga_prueba
+        )
+
+    def test_calculo_puntuacion_portero(self):
+                """
+                Prueba que .save() calcula la puntuacion_total
+                usando las stats de portero
+                """
+
+                carta_portero = Carta_jugador.objects.create(
+                    nombre="Test Portero",
+                    pais=self.pais_prueba,
+                    liga=self.liga_prueba,
+                    club=self.club_prueba,
+                    posicion='POR',
+
+
+                    salto=81,
+                    parada=81,
+                    saque=81,
+                    reflejos=81,
+                    velocidad=81,
+                    posicionamiento=81
+                )
+
+                self.assertEqual(
+                    carta_portero.puntuacion_total,
+                    93,
+                    f"El cálculo para POR falló. Se esperaba 93, pero se obtuvo {carta_portero.puntuacion_total}"
+                )
+
+    def test_calculo_puntuacion_delantero(self):
+
+        carta_delantero = Carta_jugador.objects.create(
+            nombre="Test Delantero",
+            pais=self.pais_prueba,
+            liga=self.liga_prueba,
+            club=self.club_prueba,
+            posicion='DC',
+
+
+            ritmo=81,
+            tiro=81,
+            pase=81,
+            regate=81,
+            defensa=81,
+            fisico=81,
+
+
+        )
+
+
+
+        self.assertEqual(
+            carta_delantero.puntuacion_total,
+            93,
+            f"El cálculo para Delantero falló. Se esperaba 93, pero se obtuvo {carta_delantero.puntuacion_total}"
+        )
+
+    def test_puntuacion_se_limita_a_99(self):
+
+        carta_max = Carta_jugador.objects.create(
+            nombre="Test Tope 99",
+            pais=self.pais_prueba,
+            liga=self.liga_prueba,
+            club=self.club_prueba,
+            posicion='DC',
+
+            ritmo=99,
+            tiro=99,
+            pase=99,
+            regate=99,
+            defensa=99,
+            fisico=99
+        )
+
+
+
+        self.assertEqual(
+            carta_max.puntuacion_total,
+            99,
+            f"El límite de 99 falló. Se esperaba 99, pero se obtuvo {carta_max.puntuacion_total}"
+        )
+
+    def test_puntuacion_se_limita_a_50(self):
+
+        carta_min = Carta_jugador.objects.create(
+            nombre="Test  50",
+            pais=self.pais_prueba,
+            liga=self.liga_prueba,
+            club=self.club_prueba,
+            posicion='DC',
+
+
+            ritmo=1,
+            tiro=1,
+            pase=1,
+            regate=1,
+            defensa=1,
+            fisico=1
+        )
+
+
+        self.assertEqual(
+            carta_min.puntuacion_total,
+            50,
+            f"El límite de 50 falló. Se esperaba 50, pero se obtuvo {carta_min.puntuacion_total}"
+        )
+
+
+
+
